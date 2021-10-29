082ed9ff12f9d78fdd7521fbee57cc29fa3054fa
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2021-10-29 20:11:04 +0200

    Fix error parsing error

--- src/01_structs/07_Errors.jl
+++ src/01_structs/07_Errors.jl
@@ -6,8 +6,9 @@ struct WDError <: Exception
     status::Int
     error::String
     message::String
-    function WDError(err::StatusError)
-        json = JSON3.read(err.response.body).value
-        new(err.status, json.error, json.message)
-    end
+end
+
+function WDError(err::StatusError)
+    json = JSON3.read(err.response.body)
+    WDError(err.status, json.error, json.message)
 end
